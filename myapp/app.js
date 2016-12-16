7efac1b64948b51983ecb4a8f7750c04c7172a51
JoanGarciaCedano <Joan.GC.mail@gmail.com>
2016-12-16 15:17:35 -0600

    -jo

--- myapp/app.js
+++ myapp/app.js
@@ -136,12 +136,12 @@ MongoClient.connect(url, function(err, db) {
 io.sockets.on('connection', function(socket) {
 
     //Lectura de Energía Electrica
-    gpio.setup(33, gpio.DIR_IN, leerCFE);
-    gpio.setup(13, gpio.DIR_IN, leerPANEL);
+    gpio.setup(40, gpio.DIR_IN, leerCFE);
+    gpio.setup(37, gpio.DIR_IN, leerPANEL);
 
 
     function leerCFE() {
-        gpio.read(33, function(err, value) {
+        gpio.read(40, function(err, value) {
           valorCFE = value;
           socket.emit("statusCFE", value);
           console.log("CFE: "+valorCFE);
@@ -150,7 +150,7 @@ io.sockets.on('connection', function(socket) {
     }
 
     function leerPANEL() {
-        gpio.read(13, function(err, value) {
+        gpio.read(37, function(err, value) {
           valorPANEL = value;
           socket.emit("statusPANEL", value);
           console.log("PANEL: "+valorPANEL);
